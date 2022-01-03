5038209c54418ffc1763135b9d8ff85c69846cbf
anthea <anthea@emct.com.tw>
2022-01-03 15:54:47 +0800

    新增：(R)卡片清單

---
+++ accounting-cards/Controllers/CardController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace accounting_cards.Controllers
+{
+    [RoutePrefix("api/card")]
+    public class CardController : ApiController
+    {
+        [HttpGet]
+        [Route("list")]
+        public IHttpActionResult List()
+        {
+            var result = new List<CardModel>();
+            
+            var card = new CardModel()
+            {
+                Guid = Guid.NewGuid(),
+                Name = "未分類",
+                Total = 0
+            };
+            result.Add(card);
+            
+            card = new CardModel()
+            {
+                Guid = Guid.NewGuid(),
+                Name = "飲食",
+                Total = 0
+            };
+            result.Add(card);
+            
+            return Ok(result);
+        }
+    }
+
+    public class CardModel
+    {
+        public Guid Guid { get; set; }
+        public string Name { get; set; }
+        public int Total { get; set; }
+    }
+}
